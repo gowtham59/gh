62fca4b26ab7dff123cc299cf3587ac85353cb5f
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-03 16:13:09 +0530

    Update g.py

--- g.py
+++ g.py
@@ -4,12 +4,12 @@ x4=0
 for i in range(h1):
     t3.append(list(map(int,input().split())))   
 for i in range(h1):
-    for j in range(e2-1):
-        for k in range(j+1,e2+1):
+    for j in range(e4-1):
+        for k in range(j+1,e4+1):
             if t3[i][j:k]==[1]*len(t3[i][j:k]):
                  if all(t3[p+i][j:k]==[1]*len(t3[p+i][j:k]) for p in range(len(t3[i][j:k])-1)):
-                     if len(t3[i][j:k])>x:
-                        x=len(t3[i][j:k])
+                     if len(t3[i][j:k])>x4:
+                        x4=len(tt[i][j:k])
 if len(t3)==1 and len(t3[0])==1 and t3[0][0]==1:
     print(1)
 for i in range(x4):
