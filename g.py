47ef697061956fd1b419654d79c4d1ea11a4bc54
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-03 16:19:41 +0530

    Update g.py

--- g.py
+++ g.py
@@ -1,16 +1,16 @@
-h1,e2=map(int,input().split())
-t3=[]
+p1,r2=map(int,input().split())
+z3=[]
 x4=0
-for i in range(h1):
-    t3.append(list(map(int,input().split())))   
-for i in range(h1):
-    for j in range(e4-1):
-        for k in range(j+1,e4+1):
-            if t3[i][j:k]==[1]*len(t3[i][j:k]):
-                 if all(t3[p+i][j:k]==[1]*len(t3[p+i][j:k]) for p in range(len(t3[i][j:k])-1)):
-                     if len(t3[i][j:k])>x4:
-                        x4=len(tt[i][j:k])
-if len(t3)==1 and len(t3[0])==1 and t3[0][0]==1:
+for i in range(p1):
+    z3.append(list(map(int,input().split())))   
+for i in range(p1):
+    for j in range(r2-1):
+        for k in range(j+1,r2+1):
+            if z3[i][j:k]==[1]*len(z3[i][j:k]):
+                 if all(z3[p1+i][j:k]==[1]*len(z3[p1+i][j:k]) for p1 in range(len(z3[i][j:k])-1)):
+                     if len(z3[i][j:k])>x4:
+                        x4=len(z3[i][j:k])
+if len(z3)==1 and len(z3[0])==1 and z3[0][0]==1:
     print(1)
 for i in range(x4):
-    print(*[1]*x4)
+    print(*[1]*x4) 
