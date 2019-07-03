0491de9d60f4e99cb76a7f945660416107ffdcdd
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-03 16:09:17 +0530

    Update g.py

--- g.py
+++ g.py
@@ -1,16 +1,16 @@
-ni1,mi2=map(int,input().split())
-tem=[]
-x3=0
-for i in range(ni1):
-    tem.append(list(map(int,input().split())))   
-for i in range(ni1):
-    for j in range(mi2-1):
-        for k in range(j+1,mi2+1):
-            if tem[i][j:k]==[1]*len(tem[i][j:k]):
-                 if all(tem[p+i][j:k]==[1]*len(tem[p+i][j:k]) for p in range(len(tem[i][j:k])-1)):
-                     if len(tem[i][j:k])>x:
-                        x=len(tem[i][j:k])
-if len(tem)==1 and len(tem[0])==1 and tem[0][0]==1:
+h1,e2=map(int,input().split())
+t3=[]
+x4=0
+for i in range(h1):
+    t3.append(list(map(int,input().split())))   
+for i in range(h1):
+    for j in range(e2-1):
+        for k in range(j+1,e2+1):
+            if t3[i][j:k]==[1]*len(t3[i][j:k]):
+                 if all(t3[p+i][j:k]==[1]*len(t3[p+i][j:k]) for p in range(len(t3[i][j:k])-1)):
+                     if len(t3[i][j:k])>x:
+                        x=len(t3[i][j:k])
+if len(t3)==1 and len(t3[0])==1 and t3[0][0]==1:
     print(1)
-for i in range(x3):
-    print(*[1]*x3) 
+for i in range(x4):
+    print(*[1]*x4)
